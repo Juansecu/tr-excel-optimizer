15de51000cfcce6bf3aecac81eb6dd53df96d71a
Juan Sebastian Cuello Vergara <juansebastian.cuello.contractor@bbva.com>
2023-03-07 17:36:41 -0500

    Initial commit

---
+++ src/main/java/com/juansecu/trexceloptimizer/TrExcelOptimizerApplication.java
@@ -0,0 +1,11 @@
+package com.juansecu.trexceloptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrExcelOptimizerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TrExcelOptimizerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/juansecu/trexceloptimizer/TrExcelOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansecu.trexceloptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrExcelOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
